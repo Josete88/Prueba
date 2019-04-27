1bc823e02f63bb5631a16d870841f3131d5b5fc4
Josete88 <cantalejo.inf@gmail.com>
2019-04-27 12:43:34 +0200

    Modifico Run para la máquina virtual

--- Prueba/src/run/Run.java
+++ Prueba/src/run/Run.java
@@ -3,7 +3,7 @@ package run;
 public class Run {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Run de nuevo parte 4");
+		System.out.println("Prueba Fetch y Pull para la máquina virtual");
 
 	}
 
